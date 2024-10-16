59ac4017d79d054cf60952bcca8178024bf14b04
mamyraoni <mamyraoni@gmail.com>
2024-10-16 22:38:03 +0300

    ajout method get avec salleController 😊

--- src/Controller/SalleController.php
+++ src/Controller/SalleController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Salle;
+use App\Repository\SalleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -19,5 +20,12 @@ class SalleController extends AbstractController
        $em->persist($salle);
        $em->flush();
        return $this->json($salle, 201, [], ['groups' => 'salle']);
+    }
+    #[Route('/api/salle', name: 'app_salle', methods:['GET'])]
+    public function getAllSalle(SalleRepository $salleRepository, SerializerInterface $serializer): JsonResponse
+    {
+        $salle = $salleRepository->findAll();
+        $json = $serializer->serialize($salle, 'json');
+        return new JsonResponse($json, 200, [], true);
     }
 }
