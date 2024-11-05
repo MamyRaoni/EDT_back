53d3397aef4846fbf42912ca71dbecb96025a84f
mamyraoni <mamyraoni@gmail.com>
2024-11-05 09:09:56 +0300

    last commit

--- src/Controller/GenerationEmploiDuTempsController.php
+++ src/Controller/GenerationEmploiDuTempsController.php
@@ -2,18 +2,40 @@
 
 namespace App\Controller;
 
+use App\Repository\ClasseRepository;
+use App\Repository\ContrainteRepository;
+use App\Service\TimetableService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 class GenerationEmploiDuTempsController extends AbstractController
 {
-    #[Route('/generation/emploi/du/temps', name: 'app_generation_emploi_du_temps')]
-    public function index(): JsonResponse
+    #[Route('api/generationEDT', name: 'app_generation_emploi_du_temps')]
+    public function index(TimetableService $timetableService, ContrainteRepository $contrainteRepository, ClasseRepository $classeRepository): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/GenerationEmploiDuTempsController.php',
-        ]);
+        $classes = $classeRepository->findAll(); // Remplir avec les données des classes
+        $profs_availabilities = $contrainteRepository->findAll();
+        $study_days = [
+            '2024-03-18', // Lundi
+            '2024-03-19', // Mardi
+            '2024-03-20', // Mercredi
+            '2024-03-21', // Jeudi
+            '2024-03-22'  // Vendredi
+        ]; // Remplir avec les jours d'étude
+
+        $schedule = [];
+        $success = $timetableService->generateTimetable($classes, $profs_availabilities, $study_days, $schedule);
+
+        if ($success) {
+            return $this->json([
+                'message' => 'Emploi du temps généré avec succès!',
+                'schedule' => $schedule,
+            ]);
+        } else {
+            return $this->json([
+                'message' => 'Échec de la génération de l\'emploi du temps.',
+            ], 500);
+        }
     }
 }
